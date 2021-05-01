825af05a92a014cd3f021172139d8cacb3f07887
William Martens <68499986+loneicewolf@users.noreply.github.com>
2021-05-01 14:59:24 +0200

    Create P1.java

---
+++ Pool/Projects/P1.java
@@ -0,0 +1,14 @@
+public class P1 {
+public static void x(){
+	System.out.println("\t x() called!\n");
+}
+   public static void main(String[] args) {
+	 x();
+	}
+	
+	
+}
+// $ alias MkJ='javac P1.java;java P1'
+// $ MkJ
+// # output #.  x() called!
+
